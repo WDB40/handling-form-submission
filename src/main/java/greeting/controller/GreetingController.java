37859eeedbac4e8539d43f964a7baa3d42c9ea16
WDB40 <wbrown1640@gmail.com>
2018-09-06 18:49:52 -0500

    Adding the storage of Message to the database.

--- src/main/java/greeting/controller/GreetingController.java
+++ src/main/java/greeting/controller/GreetingController.java
@@ -23,6 +23,7 @@ public class GreetingController {
 
     @PostMapping("/greeting")
     public String greetingSubmit(@ModelAttribute Greeting greeting) {
+        greetingRepository.save(greeting);
         return "result";
     }
 }
