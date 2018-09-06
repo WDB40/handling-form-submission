fa6109b7d26391d933c830f45f34b3efcf05e82b
WDB40 <wbrown1640@gmail.com>
2018-09-06 18:47:34 -0500

    Autowire GreetingRepository and remove ID input for greeting since auto generated.

--- src/main/java/greeting/controller/GreetingController.java
+++ src/main/java/greeting/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package greeting.controller;
 
 import greeting.model.Greeting;
+import greeting.model.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GreetingController {
+
+    @Autowired
+    GreetingRepository greetingRepository;
 
     @GetMapping("/greeting")
     public String greetingForm(Model model) {
